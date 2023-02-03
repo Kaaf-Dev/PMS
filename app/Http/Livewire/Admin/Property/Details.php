8baa7a8bc31a876f934aeb389f3296a707ba1114
alfakiat <waleedalfakiat@hotmail.com>
2023-02-03 15:58:25 +0300

    Update fetch property in details page

--- app/Http/Livewire/Admin/Property/Details.php
+++ app/Http/Livewire/Admin/Property/Details.php
@@ -7,18 +7,25 @@ use Livewire\Component;
 
 class Details extends Component
 {
-
-    public $property;
+    public $property_id;
 
     public function mount($property_id)
     {
-        $this->property = Property::findOrFail($property_id);
+        $this->property_id = $property_id;
     }
 
     public function render()
     {
         return view('livewire.admin.property.details')
             ->layout('layouts.admin.app');
+    }
+
+    public function getPropertyProperty()
+    {
+        return Property::whereId($this->property_id)
+            ->firstOr(function () {
+                abort(404);
+            });
     }
 
 }
