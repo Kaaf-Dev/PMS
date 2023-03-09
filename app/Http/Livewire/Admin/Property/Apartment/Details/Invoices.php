c86d115d25e498216dd355cf6d72baf3c0d4293d
alfakiat <waleedalfakiat@hotmail.com>
2023-03-09 14:49:48 +0300

    continue invoices component in apartment

--- app/Http/Livewire/Admin/Property/Apartment/Details/Invoices.php
+++ app/Http/Livewire/Admin/Property/Apartment/Details/Invoices.php
@@ -6,8 +6,18 @@ use Livewire\Component;
 
 class Invoices extends Component
 {
+
+    public $contract_id;
+
+    public function mount($contract_id)
+    {
+        $this->contract_id = $contract_id;
+    }
+
     public function render()
     {
         return view('livewire.admin.property.apartment.details.invoices');
     }
+
+//    public function get
 }
--- resources/views/livewire/admin/property/apartment/details/overview.blade.php
+++ resources/views/livewire/admin/property/apartment/details/overview.blade.php
@@ -3,7 +3,7 @@
     <div class="col-lg-12">
         @if($this->apartment->isRented)
             <!--begin::Card-->
-            <div class="card card-flush h-lg-100 pt-3 mb-5 mb-xl-10">
+            <div class="card card-flush pt-3 mb-5 mb-xl-10">
                 <!--begin::Card header-->
                 <div class="card-header">
                     <!--begin::Card title-->
@@ -26,8 +26,31 @@
                     <!--begin::Section-->
                     <div class="mb-10">
                         <!--begin::Title-->
-                        <h5 class="mb-4">بيانات المستأجر:</h5>
+                        <h5 class="mb-4">المستأجر:</h5>
                         <!--end::Title-->
+                        <!--begin::Details-->
+                        <div class="mb-4">
+                            <!--begin::Details-->
+                            <div class="d-flex align-items-center">
+                                <!--begin::Avatar-->
+                                <div class="symbol symbol-60px symbol-circle me-3">
+                                    <img alt="{{ $this->contract->user->name }}" src="{{ $this->contract->user->profilePhotoUrl }}">
+                                </div>
+                                <!--end::Avatar-->
+                                <!--begin::Info-->
+                                <div class="d-flex flex-column">
+                                    <!--begin::Name-->
+                                    <a href="{{ route('admin.users.details', ['user_id' => $this->contract->user->id]) }}" class="fs-4 fw-bold text-gray-900 text-hover-primary me-2">{{ $this->contract->user->name }}</a>
+                                    <!--end::Name-->
+                                    <!--begin::Email-->
+                                    <a href="{{ route('admin.users.details', ['user_id' => $this->contract->user->id]) }}" class="fw-semibold text-gray-600 text-hover-primary">cpr: {{ $this->contract->user->cpr }}</a>
+                                    <!--end::Email-->
+                                </div>
+                                <!--end::Info-->
+                            </div>
+                            <!--end::Details-->
+                        </div>
+                        <!--end::Details-->
                         <!--begin::Details-->
                         <div class="d-flex flex-wrap py-5">
                             <!--begin::Row-->
@@ -36,25 +59,9 @@
                                 <table class="table fs-6 fw-semibold gs-0 gy-2 gx-2 m-0">
                                     <!--begin::Row-->
                                     <tr>
-                                        <td class="text-gray-400">المستأجر:</td>
-                                        <td class="text-gray-800">{{ $this->contract->user->name }}</td>
-                                    </tr>
-                                    <!--end::Row-->
-                                    <!--begin::Row-->
-                                    <tr>
-                                        <td class="text-gray-400">CPR</td>
-                                        <td class="text-gray-800">{{ $this->contract->user->cpr }}</td>
-                                    </tr>
-                                    <!--end::Row-->
-                                    <!--begin::Row-->
-                                    <tr>
-                                        <td class="text-gray-400">البريد الإلكتروني:</td>
+                                        <td class="text-gray-400">الحالة:</td>
                                         <td class="text-gray-800">
-                                            @if($this->contract->user->email)
-                                                {{ $this->contract->user->email }}
-                                            @else
-                                                <span class="badge badge-secondary">لا يوجد</span>
-                                            @endif
+                                            <span class="badge badge-light-success">فعّال</span>
                                         </td>
                                     </tr>
                                     <!--end::Row-->
@@ -62,6 +69,12 @@
                                     <tr>
                                         <td class="text-gray-400">رقم الهاتف:</td>
                                         <td class="text-gray-800">{{ $this->contract->user->phone }}</td>
+                                    </tr>
+                                    <!--end::Row-->
+                                    <!--begin::Row-->
+                                    <tr>
+                                        <td class="text-gray-400">العقار:</td>
+                                        <td class="text-gray-800">{{ $this->contract->apartment->name }}</td>
                                     </tr>
                                     <!--end::Row-->
                                 </table>
@@ -72,12 +85,6 @@
                             <div class="flex-equal">
                                 <!--begin::Details-->
                                 <table class="table fs-6 fw-semibold gs-0 gy-2 gx-2 m-0">
-                                    <!--begin::Row-->
-                                    <tr>
-                                        <td class="text-gray-400">العقار:</td>
-                                        <td class="text-gray-800">{{ $this->contract->apartment->name }}</td>
-                                    </tr>
-                                    <!--end::Row-->
                                     <!--begin::Row-->
                                     <tr>
                                         <td class="text-gray-400">تاريخ البداية:</td>
@@ -124,129 +131,13 @@
                         </div>
                         <!--end::Section-->
                     @endif
-                    <!--begin::Section-->
-                    <div class="mb-0">
-                        <!--begin::Title-->
-                        <h5 class="mb-4">Subscribed Products:</h5>
-                        <!--end::Title-->
-                        <!--begin::Product table-->
-                        <div class="table-responsive">
-                            <!--begin::Table-->
-                            <table class="table align-middle table-row-dashed fs-6 gy-4 mb-0">
-                                <!--begin::Table head-->
-                                <thead>
-                                <!--begin::Table row-->
-                                <tr class="border-bottom border-gray-200 text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
-                                    <th class="min-w-150px">Product</th>
-                                    <th class="min-w-125px">Subscription ID</th>
-                                    <th class="min-w-125px">Qty</th>
-                                    <th class="min-w-125px">Total</th>
-                                    <th class="text-end min-w-70px">Actions</th>
-                                </tr>
-                                <!--end::Table row-->
-                                </thead>
-                                <!--end::Table head-->
-                                <!--begin::Table body-->
-                                <tbody class="fw-semibold text-gray-800">
-                                <tr>
-                                    <td>
-                                        <label class="w-150px">Basic Bundle</label>
-                                        <div class="fw-normal text-gray-600">Basic yearly bundle</div>
-                                    </td>
-                                    <td>
-                                        <span class="badge badge-light-danger">sub_4567_8765</span>
-                                    </td>
-                                    <td>1</td>
-                                    <td>$149.99 / Year</td>
-                                    <td class="text-end">
-                                        <!--begin::Action-->
-                                        <a href="#" class="btn btn-icon btn-active-light-primary w-30px h-30px" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                                            <!--begin::Svg Icon | path: icons/duotune/general/gen019.svg-->
-                                            <span class="svg-icon svg-icon-3">
-																					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-																						<path d="M17.5 11H6.5C4 11 2 9 2 6.5C2 4 4 2 6.5 2H17.5C20 2 22 4 22 6.5C22 9 20 11 17.5 11ZM15 6.5C15 7.9 16.1 9 17.5 9C18.9 9 20 7.9 20 6.5C20 5.1 18.9 4 17.5 4C16.1 4 15 5.1 15 6.5Z" fill="currentColor" />
-																						<path opacity="0.3" d="M17.5 22H6.5C4 22 2 20 2 17.5C2 15 4 13 6.5 13H17.5C20 13 22 15 22 17.5C22 20 20 22 17.5 22ZM4 17.5C4 18.9 5.1 20 6.5 20C7.9 20 9 18.9 9 17.5C9 16.1 7.9 15 6.5 15C5.1 15 4 16.1 4 17.5Z" fill="currentColor" />
-																					</svg>
-																				</span>
-                                            <!--end::Svg Icon-->
-                                        </a>
-                                        <!--begin::Menu-->
-                                        <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-6 w-200px py-4" data-kt-menu="true">
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="#" class="menu-link px-3">Pause Subscription</a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="#" class="menu-link px-3" data-kt-subscriptions-view-action="delete">Edit Subscription</a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="#" class="menu-link text-danger px-3" data-kt-subscriptions-view-action="edit">Cancel Subscription</a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                        </div>
-                                        <!--end::Menu-->
-                                        <!--end::Action-->
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <label class="w-150px">Pro Bundle</label>
-                                        <div class="fw-normal text-gray-400">Basic yearly bundle</div>
-                                    </td>
-                                    <td>
-                                        <span class="badge badge-light-danger">sub_4567_3433</span>
-                                    </td>
-                                    <td>5</td>
-                                    <td>$949.99 / Year</td>
-                                    <td class="text-end">
-                                        <!--begin::Action-->
-                                        <a href="#" class="btn btn-icon btn-active-light-primary w-30px h-30px" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                                            <!--begin::Svg Icon | path: icons/duotune/general/gen019.svg-->
-                                            <span class="svg-icon svg-icon-3">
-																					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-																						<path d="M17.5 11H6.5C4 11 2 9 2 6.5C2 4 4 2 6.5 2H17.5C20 2 22 4 22 6.5C22 9 20 11 17.5 11ZM15 6.5C15 7.9 16.1 9 17.5 9C18.9 9 20 7.9 20 6.5C20 5.1 18.9 4 17.5 4C16.1 4 15 5.1 15 6.5Z" fill="currentColor" />
-																						<path opacity="0.3" d="M17.5 22H6.5C4 22 2 20 2 17.5C2 15 4 13 6.5 13H17.5C20 13 22 15 22 17.5C22 20 20 22 17.5 22ZM4 17.5C4 18.9 5.1 20 6.5 20C7.9 20 9 18.9 9 17.5C9 16.1 7.9 15 6.5 15C5.1 15 4 16.1 4 17.5Z" fill="currentColor" />
-																					</svg>
-																				</span>
-                                            <!--end::Svg Icon-->
-                                        </a>
-                                        <!--begin::Menu-->
-                                        <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-6 w-200px py-4" data-kt-menu="true">
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="#" class="menu-link px-3">Pause Subscription</a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="#" class="menu-link px-3" data-kt-subscriptions-view-action="delete">Edit Subscription</a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="#" class="menu-link text-danger px-3" data-kt-subscriptions-view-action="edit">Cancel Subscription</a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                        </div>
-                                        <!--end::Menu-->
-                                        <!--end::Action-->
-                                    </td>
-                                </tr>
-                                </tbody>
-                                <!--end::Table body-->
-                            </table>
-                            <!--end::Table-->
-                        </div>
-                        <!--end::Product table-->
-                    </div>
-                    <!--end::Section-->
                 </div>
                 <!--end::Card body-->
             </div>
+            <!--end::Card-->
+
+            <!--begin::Card-->
+            @livewire('admin.property.apartment.details.invoices')
             <!--end::Card-->
         @else
             <!--begin::Card-->
