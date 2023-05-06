d007ec1b8e0bfcf24a7366feac146eea4bb9c34d
YazanDarAmmar1 <darammary5@gmail.com>
2023-05-06 20:46:38 +0300

    FixURLs

--- resources/views/layouts/admin/auth.blade.php
+++ resources/views/layouts/admin/auth.blade.php
@@ -6,17 +6,17 @@ www.ebepro.com
 <html lang="en">
 <!--begin::Head-->
 <head><base>
-    <title>{{ env('APP_NAME', 'BePro Team') }}</title>
+    <title>{{ env('APP_NAME', 'kaaf') }}</title>
     <meta charset="utf-8" />
     <meta name="description" content="Property Management System - BePro Team" />
     <meta name="keywords" content="bepro, team, property, management, system, quality, solution, php, laravel, livewire" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="article" />
-    <meta property="og:title" content="{{ env('APP_NAME', 'BePro Team') }}" />
-    <meta property="og:url" content="{{ env('APP_URL', 'https://www.ebepro.com') }}" />
+    <meta property="og:title" content="{{ env('APP_NAME', 'kaaf') }}" />
+    <meta property="og:url" content="{{ env('APP_URL', 'https://www.quality-solution.net') }}" />
     <meta property="og:site_name" content="Property Management System" />
-    <link rel="canonical" href="{{ env('APP_URL', 'https://www.ebepro.com') }}" />
+    <link rel="canonical" href="{{ env('APP_URL', 'https://www.quality-solution.net') }}" />
     <!--begin::Fonts(mandatory for all pages)-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
     <!--end::Fonts-->
