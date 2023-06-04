fdfa83b7cd5df76ac806e6bf6be0e71a5341537c
alfakiat <waleedalfakiat@hotmail.com>
2023-06-05 00:03:08 +0300

    Contracts list table

--- app/Http/Livewire/Admin/Contract/ListTable.php
+++ app/Http/Livewire/Admin/Contract/ListTable.php
@@ -2,12 +2,57 @@
 
 namespace App\Http\Livewire\Admin\Contract;
 
+use App\Models\Contract;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ListTable extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $ready_to_load = false;
+    public $search;
+
+    public function getListeners()
+    {
+        return [
+            'contract_added' => '$refresh',
+        ];
+    }
+
+    public function updated($property)
+    {
+        if ($property == 'search') {
+            $this->resetPage();
+        }
+
+    }
+
     public function render()
     {
-        return view('livewire.admin.contract.list-table');
+        $contracts = ($this->ready_to_load) ? $this->loadContracts() : [];
+        $view_data = [
+            'contracts' => $contracts,
+        ];
+
+        return view('livewire.admin.contract.list-table', $view_data);
+    }
+
+    public function load()
+    {
+        $this->ready_to_load = true;
+    }
+
+    public function loadContracts()
+    {
+        return Contract::orderBy('id', 'desc')
+            ->paginate();
+    }
+
+    public function showAddContract()
+    {
+        $this->emit('show-contract-new-modal', []);
     }
 }
--- resources/views/livewire/admin/contract/list-table.blade.php
+++ resources/views/livewire/admin/contract/list-table.blade.php
@@ -1,3 +1,172 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <!--begin::Card-->
+    <div wire:init="load" class="card">
+        <!--begin::Card header-->
+        <div class="card-header border-0 pt-6">
+            <!--begin::Card title-->
+            <div class="card-title">
+                <!--begin::Search-->
+{{--                <div class="d-flex align-items-center position-relative my-1">--}}
+{{--                    <!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->--}}
+{{--                    <span class="svg-icon svg-icon-1 position-absolute ms-6">--}}
+{{--                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"--}}
+{{--                         xmlns="http://www.w3.org/2000/svg">--}}
+{{--                        <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546"--}}
+{{--                              height="2" rx="1"--}}
+{{--                              transform="rotate(45 17.0365 15.1223)"--}}
+{{--                              fill="currentColor"/>--}}
+{{--                        <path--}}
+{{--                            d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"--}}
+{{--                            fill="currentColor"/>--}}
+{{--                    </svg>--}}
+{{--                </span>--}}
+{{--                    <!--end::Svg Icon-->--}}
+{{--                    <input wire:model="search" type="text" class="form-control form-control-solid w-250px ps-14"--}}
+{{--                           placeholder="Search"/>--}}
+{{--                </div>--}}
+                <!--end::Search-->
+            </div>
+            <!--begin::Card title-->
+            <!--begin::Card toolbar-->
+            <div class="card-toolbar">
+                <!--begin::Toolbar-->
+                <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
+                    <!--begin::Add user-->
+                    <button wire:click="showAddContract" type="button" class="btn btn-primary" data-bs-toggle="modal">
+                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+                        <span class="svg-icon svg-icon-2">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <rect opacity="0.5" x="11.364" y="20.364" width="16"
+                                      height="2" rx="1"
+                                      transform="rotate(-90 11.364 20.364)"
+                                      fill="currentColor"/>
+                                <rect x="4.36396" y="11.364" width="16" height="2" rx="1"
+                                      fill="currentColor"/>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->تسجيل عقد
+                    </button>
+                    <!--end::Add user-->
+                </div>
+                <!--end::Toolbar-->
+
+            </div>
+            <!--end::Card toolbar-->
+        </div>
+        <!--end::Card header-->
+        <!--begin::Card body-->
+        <div class="card-body py-4">
+
+            <div wire:loading.class="table-loading" wire:target="load" class="table-responsive">
+                <div class="table-loading-message">
+                    الرجاء الإنتظار...
+                </div>
+
+                <!--begin::Table-->
+                <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users">
+                    <!--begin::Table head-->
+                    <thead>
+                    <!--begin::Table row-->
+                    <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
+                        <th class="w-10px pe-2">
+                            #
+                        </th>
+                        <th class="min-w-125px">المستأجر</th>
+                        <th class="min-w-125px">العقار</th>
+                        <th class="min-w-125px">الوحدة</th>
+                        <th class="min-w-125px">الحالة</th>
+                        <th class="min-w-125px">بداية العقد</th>
+                        <th class="min-w-125px">نهاية العقد</th>
+                        <th class="text-end min-w-100px">العمليات</th>
+                    </tr>
+                    <!--end::Table row-->
+                    </thead>
+                    <!--end::Table head-->
+                    <!--begin::Table body-->
+                    <tbody class="text-gray-600 fw-semibold">
+
+                    @if($contracts)
+
+                        @forelse($contracts as $contract)
+                            <!--begin::Table row-->
+                            <tr>
+                                <!--begin::Checkbox-->
+                                <td>
+                                    {{ $contract->id }}
+                                </td>
+                                <!--end::Checkbox-->
+                                <!--begin::User=-->
+                                <td>
+                                    <a href="{{ route('admin.users.details', ['user_id' => $contract->user_id]) }}" class="text-gray-800 text-hover-primary mb-1">
+                                        {{ $contract->user->name }}
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{ route('admin.property.details', ['property_id' => $contract->apartment->property->id]) }}" class="text-gray-800 text-hover-primary mb-1">
+                                        {{ $contract->apartment->property->name }}
+                                    </a>
+                                </td>
+                                <!--end::User=-->
+                                <!--begin::User=-->
+                                <td>
+                                    <a href="{{ route('admin.property.apartment.details', ['property_id' => $contract->apartment->property->id, 'apartment_id' => $contract->apartment->id]) }}" class="text-gray-800 text-hover-primary mb-1">
+                                        {{ $contract->apartment->name }}
+                                    </a>
+                                </td>
+                                <!--end::User=-->
+                                <!--begin::Role=-->
+                                <td>
+                                    <span class="badge badge-light-{{ $contract->activeStatusClass }} fs-7 fw-bold">
+                                        {{ $contract->activeStatusString }}
+                                    </span>
+                                </td>
+
+                                <!--end::Role=-->
+                                <!--begin::Joined-->
+                                <td>{{ ($contract->start_at)? $contract->start_at->format('Y/m/d') : '-' }}</td>
+                                <td>{{ ($contract->end_at)? $contract->end_at->format('Y/m/d') : '-' }}</td>
+                                <!--begin::Joined-->
+                                <!--begin::Action=-->
+                                <td class="text-end">
+                                    <a href="{{ route('admin.contracts.details', ['contract_id' => $contract->id]) }}"
+                                       class="btn btn-light btn-active-light-primary btn-sm">
+                                        <i class="fa-solid fa-gear"></i>
+                                    </a>
+                                </td>
+                                <!--end::Action=-->
+                            </tr>
+                            <!--end::Table row-->
+                        @empty
+                            <tr>
+                                <td colspan="100%">
+
+                                    <div class="d-flex flex-column flex-center">
+                                        <img src="{{ asset('admin-assets/media/illustrations/sigma-1/5.png') }}"
+                                             class="mw-350px">
+                                        <div class="fs-3 fw-bolder text-dark mb-4">No data found.</div>
+                                        <div class="fs-6"></div>
+                                    </div>
+
+
+                                </td>
+                            </tr>
+                        @endforelse
+
+                    @endif
+
+                    </tbody>
+                    <!--end::Table body-->
+                </table>
+                <!--end::Table-->
+            </div>
+
+            @if($contracts)
+                {{ $contracts->links() }}
+            @endif
+
+        </div>
+        <!--end::Card body-->
+    </div>
+    <!--end::Card-->
 </div>
