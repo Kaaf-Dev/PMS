d683f5c8363d006f18175dc29fbac0eddc494121
alfakiat <waleedalfakiat@hotmail.com>
2023-07-19 17:44:35 +0300

    Add lawyer dashboard

---
+++ app/Http/Livewire/Layoutes/Lawyer/AppSidebar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Layoutes\Lawyer;
+
+use Livewire\Component;
+
+class AppSidebar extends Component
+{
+    public function render()
+    {
+        return view('livewire.layoutes.lawyer.app-sidebar');
+    }
+}
---
+++ resources/views/livewire/layoutes/lawyer/app-header.blade.php
@@ -0,0 +1,128 @@
+<div id="kt_app_header" class="app-header" data-kt-sticky="true" data-kt-sticky-activate-="true" data-kt-sticky-name="app-header-sticky" data-kt-sticky-offset="{default: '200px', lg: '300px'}">
+    <!--begin::Header container-->
+    <div class="app-container container-xxl d-flex align-items-stretch justify-content-between" id="kt_app_header_container">
+        <!--begin::Header wrapper-->
+        <div class="app-header-wrapper d-flex flex-grow-1 align-items-stretch justify-content-between" id="kt_app_header_wrapper">
+            <!--begin::Logo wrapper-->
+            <div class="app-header-logo d-flex flex-shrink-0 align-items-center justify-content-between justify-content-lg-center">
+                <!--begin::Logo wrapper-->
+                <button class="btn btn-icon btn-color-gray-600 btn-active-color-primary ms-n3 me-2 d-flex d-lg-none" id="kt_app_sidebar_toggle">
+                    <i class="ki-outline ki-abstract-14 fs-2"></i>
+                </button>
+                <!--end::Logo wrapper-->
+                <!--begin::Logo image-->
+                <a href="{{ route('lawyer.dashboard') }}">
+                    <img alt="Logo" src="{{ asset('lawyer-assets/media/logos/default-small.svg') }}" class="h-30px h-lg-40px theme-light-show" />
+                    <img alt="Logo" src="{{ asset('lawyer-assets/media/logos/default-small-dark.svg') }}" class="h-30px h-lg-40px theme-dark-show" />
+                </a>
+                <!--end::Logo image-->
+            </div>
+            <!--end::Logo wrapper-->
+
+            <!--begin::Navbar-->
+            <div class="app-navbar flex-shrink-0">
+                <!--begin::User menu-->
+                <div class="app-navbar-item ms-3 ms-lg-5" id="kt_header_user_menu_toggle">
+                    <!--begin::Menu wrapper-->
+                    <div class="cursor-pointer symbol symbol-35px symbol-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+                        <img class="symbol symbol-circle symbol-35px symbol-md-40px" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                    </div>
+                    <!--begin::User account menu-->
+                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-3">
+                            <div class="menu-content d-flex align-items-center px-3">
+                                <!--begin::Avatar-->
+                                <div class="symbol symbol-50px me-5">
+                                    <img alt="Logo" src="{{ Auth::user()->profile_photo_url }}" />
+                                </div>
+                                <!--end::Avatar-->
+                                <!--begin::Username-->
+                                <div class="d-flex flex-column">
+                                    <div class="fw-bold d-flex align-items-center fs-5">
+                                        {{ Auth::user()->name }}
+                                    </div>
+                                    <a class="fw-semibold text-muted text-hover-primary fs-7">{{ Auth::user()->email }}</a>
+                                </div>
+                                <!--end::Username-->
+                            </div>
+                        </div>
+                        <!--end::Menu item-->
+                        <!--begin::Menu separator-->
+                        <div class="separator my-2"></div>
+                        <!--end::Menu separator-->
+                        <!--begin::Menu item-->
+{{--                        <div class="menu-item px-5">--}}
+{{--                            <a href="#" class="menu-link px-5">الملف الشخصي</a>--}}
+{{--                        </div>--}}
+                        <!--end::Menu item-->
+
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
+                            <a href="#" class="menu-link px-5">
+												<span class="menu-title position-relative">المظهر
+												<span class="ms-5 position-absolute translate-middle-y top-50 end-0">
+													<i class="ki-outline ki-night-day theme-light-show fs-2"></i>
+													<i class="ki-outline ki-moon theme-dark-show fs-2"></i>
+												</span></span>
+                            </a>
+                            <!--begin::Menu-->
+                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-title-gray-700 menu-icon-gray-500 menu-active-bg menu-state-color fw-semibold py-4 fs-base w-150px" data-kt-menu="true" data-kt-element="theme-mode-menu">
+                                <!--begin::Menu item-->
+                                <div class="menu-item px-3 my-0">
+                                    <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="light">
+														<span class="menu-icon" data-kt-element="icon">
+															<i class="ki-outline ki-night-day fs-2"></i>
+														</span>
+                                        <span class="menu-title">فاتح</span>
+                                    </a>
+                                </div>
+                                <!--end::Menu item-->
+                                <!--begin::Menu item-->
+                                <div class="menu-item px-3 my-0">
+                                    <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="dark">
+														<span class="menu-icon" data-kt-element="icon">
+															<i class="ki-outline ki-moon fs-2"></i>
+														</span>
+                                        <span class="menu-title">داكن</span>
+                                    </a>
+                                </div>
+                                <!--end::Menu item-->
+                                <!--begin::Menu item-->
+                                <div class="menu-item px-3 my-0">
+                                    <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="system">
+														<span class="menu-icon" data-kt-element="icon">
+															<i class="ki-outline ki-screen fs-2"></i>
+														</span>
+                                        <span class="menu-title">تلقائي</span>
+                                    </a>
+                                </div>
+                                <!--end::Menu item-->
+                            </div>
+                            <!--end::Menu-->
+                        </div>
+                        <!--end::Menu item-->
+                        <!--begin::Menu item-->
+                        <div class="menu-item px-5">
+                            <a href="{{ route('lawyer.auth.logout') }}" class="menu-link px-5">تسجيل الخروج</a>
+                        </div>
+                        <!--end::Menu item-->
+                    </div>
+                    <!--end::User account menu-->
+                    <!--end::Menu wrapper-->
+                </div>
+                <!--end::User menu-->
+                <!--begin::Header menu toggle-->
+                <div class="app-navbar-item d-lg-none ms-2 me-n3" title="Show header menu">
+                    <div class="btn btn-icon btn-custom btn-active-color-primary btn-color-gray-700 w-35px h-35px w-md-40px h-md-40px" id="kt_app_header_menu_toggle">
+                        <i class="ki-outline ki-text-align-left fs-1"></i>
+                    </div>
+                </div>
+                <!--end::Header menu toggle-->
+            </div>
+            <!--end::Navbar-->
+        </div>
+        <!--end::Header wrapper-->
+    </div>
+    <!--end::Header container-->
+</div>
