9cdce32a86f3db2baf7299a5d91fb1106dc03da5
alfakiat <waleedalfakiat@hotmail.com>
2023-08-22 00:05:57 +0300

    Attach with tickets

--- app/Http/Livewire/Admin/Ticket/Details/AddReply.php
+++ app/Http/Livewire/Admin/Ticket/Details/AddReply.php
@@ -6,19 +6,25 @@ use App\Models\Ticket;
 use App\Traits\WithAlert;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class AddReply extends Component
 {
 
+    use WithFileUploads;
     use WithAlert;
 
     public $ticket_id;
     public $reply;
 
+    public $attachments;
+    public $attachment;
+
     public function rules()
     {
         return [
             'reply' => 'required|string|max:2000',
+            'attachments.*' => 'required|mimes:png,jpg,jpeg|max:1024',
         ];
     }
 
@@ -46,6 +52,24 @@ class AddReply extends Component
         return view('livewire.admin.ticket.details.add-reply');
     }
 
+    public function updatedAttachment()
+    {
+        $this->validate([
+            'attachment' => 'required|mimes:png,jpg,jpeg|max:10240', // 1MB Max
+        ]);
+        $this->attachments[md5(time())] = $this->attachment;
+        $this->reset([
+            'attachment',
+        ]);
+    }
+
+    public function cancelAttachment($attachment)
+    {
+        if (isset($this->attachments[$attachment])) {
+            unset($this->attachments[$attachment]);
+        }
+    }
+
     public function getTicketProperty()
     {
         return Ticket::findOrFail($this->ticket_id);
@@ -61,6 +85,14 @@ class AddReply extends Component
         ]);
 
         if ($reply) {
+            $attachments = [];
+            foreach ($validated_data['attachments'] ?? [] as $attachment) {
+                $attachments[] = [
+                    'path' =>  $attachment->store('reply-' . md5($reply->id), 'ticket_attachments'),
+                    'file_name' => $attachment->getClientOriginalName(),
+                ];
+            }
+            $reply->attachments()->createMany($attachments);
             $this->ticket->touch();
             $this->resetInputs();
             $this->emit('reply-added');
@@ -73,6 +105,8 @@ class AddReply extends Component
     {
         $this->reset([
             'reply',
+            'attachments',
+            'attachment',
         ]);
     }
 }
