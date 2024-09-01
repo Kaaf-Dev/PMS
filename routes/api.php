e2dcc526cd493d80feddc585e750ea954153d429
yazan <darammary5@gmail.com>
2024-09-01 11:12:05 +0300

    PayUsingBenefitPayJob

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,9 @@ use App\Http\Controllers\PaymentCallbackApi;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+});
+Route::group([], function () {
     Route::any('benefit-kaaf-response', [PaymentCallbackApi::class, 'benefitKaafResponse'])->name('benefit.kaaf.response');
     Route::any('benefit-eslah-response', [PaymentCallbackApi::class, 'benefitEslahResponse'])->name('benefit.eslah.response');
 });
