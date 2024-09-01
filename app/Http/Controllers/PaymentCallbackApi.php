4bffaa31b372ee15c43f50b26d26ae65f7011464
yazan <darammary5@gmail.com>
2024-09-01 09:47:51 +0300

    PayUsingBenefitPay

--- app/Http/Controllers/PaymentCallbackApi.php
+++ app/Http/Controllers/PaymentCallbackApi.php
@@ -116,6 +116,8 @@ class PaymentCallbackApi extends Controller
     public function benefitEslahResponse()
     {
         $request = request();
+        info($request);
+
         // Check if x-foo-signature header exists
         if ($request->hasHeader('x-foo-signature')) {
             $foo_signature = $request->header("x-foo-signature");
