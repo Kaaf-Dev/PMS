769aa750f5885c073b1813ecdd8156a1fb14511e
yazan <darammary5@gmail.com>
2024-11-11 15:35:59 +0300

    ReportPdf

--- resources/views/livewire/admin/report/form/executive-summary.blade.php
+++ resources/views/livewire/admin/report/form/executive-summary.blade.php
@@ -22,22 +22,39 @@
         <!--begin::Actions-->
         <div class="text-center">
             <button wire:click="closeModal" type="button" class="btn btn-light me-3">
-                عودة
+                <i class="bi bi-arrow-down"></i> عودة
             </button>
 
-            <button type="submit" id="kt_modal_new_target_submit" class="btn btn-primary">
+            <!-- Excel Download Button -->
+            <button type="button" id="kt_modal_export_excel" class="btn btn-success" wire:click="exportExcel">
                 <!--begin::Indicator label-->
-                <span wire:loading.remove wire:target="export">تصدير التقرير</span>
+                <span wire:loading.remove wire:target="exportExcel">
+                    <i class="bi bi-file-earmark-spreadsheet"></i> تصدير إلى Excel
+                </span>
                 <!--end::Indicator label-->
                 <!--begin::Indicator progress-->
-                <span wire:loading wire:target="export">
-                    الرجاء الانتظار
-					<span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                <span wire:loading wire:target="exportExcel">
+                    <i class="bi bi-hourglass-split"></i> الرجاء الانتظار
+                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                </span>
+                <!--end::Indicator progress-->
+            </button>
+
+            <!-- PDF Download Button -->
+            <button type="button" id="kt_modal_export_pdf" class="btn btn-warning" wire:click="exportPDF">
+                <!--begin::Indicator label-->
+                <span wire:loading.remove wire:target="exportPDF">
+                    <i class="bi bi-file-earmark-pdf"></i> تصدير إلى PDF
+                </span>
+                <!--end::Indicator label-->
+                <!--begin::Indicator progress-->
+                <span wire:loading wire:target="exportPDF">
+                    <i class="bi bi-hourglass-split"></i> الرجاء الانتظار
+                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
                 </span>
                 <!--end::Indicator progress-->
             </button>
         </div>
         <!--end::Actions-->
     </form>
-
 </div>
--- resources/views/livewire/admin/report/form/lawyer-cases.blade.php
+++ resources/views/livewire/admin/report/form/lawyer-cases.blade.php
@@ -9,7 +9,7 @@
             </label>
             <!--end::Label-->
 
-            <select wire:model.defer="selected_year" class="form-select">
+            <select wire:model.defer="selected_lawyer" class="form-select">
                 <option value="">الجميع</option>
                 @foreach($this->lawyers ?? [] as $lawyer)
                     <option value="{{ $lawyer->id }}">{{ $lawyer->name }}</option>
@@ -22,17 +22,35 @@
         <!--begin::Actions-->
         <div class="text-center">
             <button wire:click="closeModal" type="button" class="btn btn-light me-3">
-                عودة
+                <i class="bi bi-arrow-down"></i> عودة
             </button>
 
-            <button type="submit" id="kt_modal_new_target_submit" class="btn btn-primary">
+            <!-- Excel Download Button -->
+            <button type="button" id="kt_modal_export_excel" class="btn btn-success" wire:click="exportExcel">
                 <!--begin::Indicator label-->
-                <span wire:loading.remove wire:target="export">تصدير التقرير</span>
+                <span wire:loading.remove wire:target="exportExcel">
+                    <i class="bi bi-file-earmark-spreadsheet"></i> تصدير إلى Excel
+                </span>
                 <!--end::Indicator label-->
                 <!--begin::Indicator progress-->
-                <span wire:loading wire:target="export">
-                    الرجاء الانتظار
-					<span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                <span wire:loading wire:target="exportExcel">
+                    <i class="bi bi-hourglass-split"></i> الرجاء الانتظار
+                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                </span>
+                <!--end::Indicator progress-->
+            </button>
+
+            <!-- PDF Download Button -->
+            <button type="button" id="kt_modal_export_pdf" class="btn btn-warning" wire:click="exportPDF">
+                <!--begin::Indicator label-->
+                <span wire:loading.remove wire:target="exportPDF">
+                    <i class="bi bi-file-earmark-pdf"></i> تصدير إلى PDF
+                </span>
+                <!--end::Indicator label-->
+                <!--begin::Indicator progress-->
+                <span wire:loading wire:target="exportPDF">
+                    <i class="bi bi-hourglass-split"></i> الرجاء الانتظار
+                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
                 </span>
                 <!--end::Indicator progress-->
             </button>
