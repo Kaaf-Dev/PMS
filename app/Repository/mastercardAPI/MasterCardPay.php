69a093646c736fb8f6616fed50f7a0a9f8ab795c
yazan <yazan.darammar@yahoo.com>
2025-05-18 10:04:08 +0300

    fixes

--- app/Repository/mastercardAPI/MasterCardPay.php
+++ app/Repository/mastercardAPI/MasterCardPay.php
@@ -139,7 +139,7 @@ class MasterCardPay
                 'response' => $payResponse,
             ];
         }
-
+        info($payResponse);
         $this->setErrorMsg($payResponse['response']['acquirerMessage']);
         return [
             'status' => false,
