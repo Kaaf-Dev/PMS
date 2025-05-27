0edda66d19ed4dc3654d4660d59754ac20d7e841
yazan <yazan.darammar@yahoo.com>
2025-05-27 15:15:10 +0300

    fixes

--- resources/views/livewire/user/payment/pay-invoice.blade.php
+++ resources/views/livewire/user/payment/pay-invoice.blade.php
@@ -210,7 +210,6 @@
 
 
             <script wire:ignore>
-                window.livewire.off('benefit-by-benefit-pay');
                 window.livewire.on('benefit-by-benefit-pay', function (params) {
                     console.log(1)
                     InApp.open(params,
