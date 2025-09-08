0922ad0d029c4f6d6c9435907abd1e42c7301f46
Ahmed-Hani99 <Ahmad.hane899@gmail.com>
2025-09-08 10:02:38 +0300

    fix payment by benefit

--- resources/views/livewire/user/payment/pay.blade.php
+++ resources/views/livewire/user/payment/pay.blade.php
@@ -208,21 +208,27 @@
         </form>
     @endif
 
-        <script wire:ignore>
-            window.livewire.on('benefit-by-benefit-pay', function (params) {
-                console.log(1)
-                InApp.open(params,
-                    function (success) {
-                        window.livewire.emit('benefit-pay-success-payment', success);
-                    },
-                    function (error) {
-                        console.log(error)
-                    },
-                    function (cancel) {
-                        console.log(cancel)
-                    },
-                )
-            })
-        </script>
+        <div wire:ignore>
+            <script>
+                document.addEventListener("livewire:load", () => {
+                    window.livewire.on('benefit-by-benefit-pay', function (params) {
+                        console.log(1);
+                        InApp.open(
+                            params,
+                            function (success) {
+                                window.livewire.emit('benefit-pay-success-payment', success);
+                            },
+                            function (error) {
+                                console.log(error);
+                            },
+                            function (cancel) {
+                                console.log(cancel);
+                            },
+                        );
+                    });
+                });
+            </script>
+        </div>
+
     </div>
 </div>
